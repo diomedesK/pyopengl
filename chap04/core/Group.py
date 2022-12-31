3bdf52ac788db013ee88d2d4a515e7f73b8f443a
Yashiro <doomed2ashes@yandex.com>
2022-12-30 22:04:09 -0300

    delete chap4

--- chap04/core/Group.py
+++
@@ -1,8 +0,0 @@
-from core.Object3D import Object3D
-
-class Group(Object3D):
-    """A general container for the scene graph"""
-
-    def __init__(self):
-        super(Group, self).__init__()
-        
